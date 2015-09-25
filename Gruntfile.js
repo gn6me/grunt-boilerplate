4e12c52acfba4565918e309462be75e2dead5373
unknown <tsleeper@CMK-DT-005.cmk.local>
2015-09-25 14:05:56 -0400

    Fixed Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,32 +3,19 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        jshint: {
-            files: ['Gruntfile.js','js/libs/*.js']
-            options: {
-                globals: {
-                    jQuery: true
-                }
-            }
-        },
-
         concat: {
-            js: {
                 dist: {
-                    src: ['js/libs/*.js']
+                    src: ['js/libs/*.js'],
                     dest: 'js/build/production.js',
                 },
 
-            },
         },
 
         uglify: {
-            js: {
                 build: {
                     src: 'js/build/production.js',
                     dest: 'js/production.min.js',
                 },
-            },
         },
 
         sass: {
@@ -38,18 +25,11 @@ module.exports = function(grunt) {
                 },
                 files: {
                     'css/build/template.css': 'css/template.scss'
-                }
-            }
+                },
+            },
         },
 
         watch: {
-            jshint: {
-                files: ['<%= jshint.files %>'],
-                tasks: ['jshint'],
-                options: {
-                    spawn: false,
-                },
-            },
             scripts: {
                 files: ['js/libs/*.js'],
                 tasks: ['concat', 'uglify'],
@@ -62,14 +42,14 @@ module.exports = function(grunt) {
                 tasks: ['sass'],
                 options: {
                     spawn: false,
-                }
-            }
-        }
+                },
+            },
+        },
 
     });
 
     require('load-grunt-tasks')(grunt);
 
-    grunt.registerTask('default', ['jshint','concat', 'uglify', 'sass', 'watch']);
+    grunt.registerTask('default', ['concat', 'uglify', 'sass', 'watch']);
 
 };
