a28459b92407e8ee5b08a59fa70b5dfe73f87337
unknown <tsleeper@CMK-DT-005.cmk.local>
2015-09-25 11:07:26 -0400

    no message

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {   
+            dist: {
+                src: [
+                    'js/libs/*.js', // All JS in the libs folder
+                ],
+                dest: 'js/build/production.js',
+            }
+        },
+
+        uglify: {
+            build: {
+                src: 'js/build/production.js',
+                dest: 'js/production.min.js'
+            }
+        },
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'css/build/template.css': 'css/template.scss'
+                }
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: true,
+            },
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false,
+                },
+            },
+            css: {
+                files: ['css/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                }
+            }
+        }
+
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['concat', 'uglify', 'sass', 'watch']);
+
+};
